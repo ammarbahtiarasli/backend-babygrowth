2522824d1e3e0f3f6cc2b20dc72941595f1b7c48
rivaannn <mrivans2002@gmail.com>
2024-05-30 17:07:11 +0700

    Fixed All Environment

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,17 +5,27 @@ const authRoutes = [
     method: "POST",
     path: "/register",
     options: {
-        auth: false,
-        },
+      auth: false,
+    },
     handler: authController.register,
   },
   {
     method: "POST",
     path: "/login",
     options: {
-        auth: false,
-        },
+      auth: false,
+    },
     handler: authController.login,
+  },
+  {
+    method: "PUT",
+    path: "/editProfile",
+    handler: authController.editProfile,
+  },
+  {
+    method: "POST",
+    path: "/logout",
+    handler: authController.logout,
   },
 ];
 
